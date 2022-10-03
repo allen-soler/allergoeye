e69c2196c51f4067be7b2da97cbc4f05881c23ad
Jorge Allen <jorgeallen@Jorges-Mac-mini.local>
2022-10-03 21:07:20 +0200

    login working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,16 +7,18 @@ const Header = (props) => {
     const ctx = useContext(AuthContext)
     const langOnClick = (e) => {
         e.preventDefault();
-    } 
+    }
 
     return (
         <header id="home" className={module.header}>
-            <div className={module.langMenu}>
-                <a id="EN" onClick={langOnClick} href="EN">EN</a>
-                <a id="DE" onClick={langOnClick} href="DE">DE</a>
-            </div>
-            <div className={module.user}>
-                <p>{ctx.user}</p>
+            <div className={module.wrapper}>
+                <div className={module.langMenu}>
+                    <a id="EN" onClick={langOnClick} href="EN">EN</a>
+                    <a id="DE" onClick={langOnClick} href="DE">DE</a>
+                </div>
+                <div className={module.user}>
+                    <p>{ctx.user}</p>
+                </div>
             </div>
             <Fragment>
                 {props.children}
